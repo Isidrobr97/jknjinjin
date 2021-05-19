38f7b7bcd6f61b67efd797f46d4e24d324dbede1
Isidrobr97 <84431353+Isidrobr97@users.noreply.github.com>
2021-05-19 00:27:37 -0400

    Add files via upload

---
+++ calculadora.java
@@ -0,0 +1,61 @@
+package numeros;
+
+import java.util.Scanner;
+
+public class calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=0, b=0, res=0;
+		String sel=null;
+      Scanner tec=new Scanner(System.in);
+		System.out.println("Selecione la opcion\n");
+		System.out.println("+: Suma"
+				+"\n-: Resta"
+		        +"*\n*: Multiplicación"
+				+"\n/: División");
+		sel=tec.next();
+		switch(sel){
+		case "+":
+			System.out.println("ha seleccionado suma\n");
+			System.out.println("ingrese el primer número");
+			a=tec.nextInt();
+			System.out.println("ingrese el segundo número");
+			b=tec.nextInt();
+			res=a+b;
+			System.out.println(a+"+"+b+"="+res);
+			break;
+		case "-":
+			System.out.println("ha seleccionado resta\n");
+			System.out.println("ingrese el primer número");
+			a=tec.nextInt();
+			System.out.println("ingrese el segundo número");
+			b=tec.nextInt();
+			res=a-b;
+			System.out.println(a+"-"+b+"="+res);
+			break;
+		case "*":
+			System.out.println("ha seleccionado Multiplicación\n");
+			System.out.println("ingrese el primer número");
+			a=tec.nextInt();
+			System.out.println("ingrese el segundo número");
+			b=tec.nextInt();
+			res=a*b;
+			System.out.println(a+"*"+b+"="+res);
+			break;
+		case "/":
+			System.out.println("ha seleccionado División\n");
+			System.out.println("ingrese el primer número");
+			a=tec.nextInt();
+			System.out.println("ingrese el segundo número");
+			b=tec.nextInt();
+			res=a/b;
+			System.out.println(a+"/"+b+"="+res);
+			break;
+			default:
+				System.out.println("Fallo de operación");
+	     }
+	}
+
+}
+	
